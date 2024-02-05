5588a0e33eec21a7b3bd5f6ac82d5a495177cbe4
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-02-04 17:42:20 -0800

    Change to pf2Feats.js

--- app/routes/pf2-feats.js
+++ app/routes/pf2-feats.js
@@ -8,7 +8,7 @@ export default Route.extend(DefaultRoute, {
     
     model: function() {
         let api = this.gameApi;
-        return api.requestOne('pf2_feats_all');
+        return api.requestOne('pf2Feats');
     },
     
     afterModel: function() {

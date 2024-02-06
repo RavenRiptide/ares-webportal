5e91a311c5531b14c7477e712c27cba0bb8733b0
emblemofeamu <74258085+emblemofeamu@users.noreply.github.com>
2024-02-06 07:58:35 -0800

    Split feat routes by type, each gets heir own handler

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -2,5 +2,9 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
-router.route('pf2-feats', { path: '/pf2/feats' });
+router.route('pf2-feats-charclass', { path: '/pf2/feats/charclass' });
+router.route('pf2-feats-ancestry', { path: '/pf2/feats/ancestry' });
+router.route('pf2-feats-general', { path: '/pf2/feats/general' });
+router.route('pf2-feats-skill', { path: '/pf2/feats/skill' });
+router.route('pf2-feats-dedication', { path: '/pf2/feats/dedication' });
 }
--- app/routes/pf2-feats-skill.js
+++ app/routes/pf2-feats-skill.js
@@ -4,15 +4,10 @@ import DefaultRoute from 'ares-webportal/mixins/default-route';
 
 export default Route.extend(DefaultRoute, {
     gameApi: service(),
-    headData: service(),
     
     model: function() {
         let api = this.gameApi;
-        return api.requestOne('pf2Feats');
+        return api.requestOne('pf2FeatsSkill');
     },
-    
-    afterModel: function() {
-      this.set('headData.robotindex', true);
-    }
         
 });
